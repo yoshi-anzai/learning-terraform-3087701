9681efae39dcd3fc2c1fbe6285fb96b386a99c11
Yoshi <105471298+yoshi-anzai@users.noreply.github.com>
2025-07-13 17:43:25 +0100

    Fix secuirty group

--- main.tf
+++ main.tf
@@ -26,10 +26,10 @@ module "blog_sg" {
   vpc_id = data.aws_vpc.default.id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cid_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
-  egress_cid_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "blog" {
