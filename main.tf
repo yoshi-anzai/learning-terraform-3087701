7832507cd04cef9b9eedf74e15cde2bfdb451ae2
Yoshi <105471298+yoshi-anzai@users.noreply.github.com>
2025-07-14 13:24:52 +0100

    Replace instance with autoscaling instance

--- main.tf
+++ main.tf
@@ -48,18 +48,24 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "Learning Terraform"
-  }
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  vpc_security_group_ids = [module.blog_sg.vpc_security_group_id]
+  
+  image_id = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
