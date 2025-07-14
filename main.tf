2ec0e99779bf5d798af20490fcf48d8892e8b6c1
Yoshi <105471298+yoshi-anzai@users.noreply.github.com>
2025-07-14 13:31:52 +0100

    Fix autoscaling

--- main.tf
+++ main.tf
@@ -58,8 +58,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_groups.blog.target_id
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
   
   image_id = data.aws_ami.app_ami.id
   instance_type = var.instance_type
