e49622ebe8e9794602f664e294b3fc7d79b44349
Yoshi <105471298+yoshi-anzai@users.noreply.github.com>
2025-07-14 13:35:44 +0100

    Fix

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "blog_sg" {
 }
 
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.0.1"
   
@@ -89,7 +89,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = module.autoscaling.blog.id
+      target_id        = module.blog_autoscaling.id
     }
   }
 
