03f4c154ee7d92bd4e12d0619776b0cca5bf09e7
Yoshi <105471298+yoshi-anzai@users.noreply.github.com>
2025-07-14 13:45:00 +0100

    Disable deletion protection

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ module "blog_alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
+  enable_deletion_protection = false
   
   
   listeners = {
