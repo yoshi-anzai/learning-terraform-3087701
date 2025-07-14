1ff7b553c7e95c98cf9a82b51ea5bdd8b992237d
Yoshi <105471298+yoshi-anzai@users.noreply.github.com>
2025-07-14 10:06:58 +0100

    Fix SG in LB module

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     http-https-redirect = {
